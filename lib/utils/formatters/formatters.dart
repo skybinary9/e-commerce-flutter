1f91da4cf40ca61313234ef5081d148811edad28
skybinary9 <skybinary9@gmail.com>
2025-10-24 16:36:50 +0500

    formate file

---
+++ lib/utils/formatters/formatters.dart
@@ -0,0 +1,8 @@
+import 'package:intl/intl.dart';
+
+class MegamartFormatter{
+  static String formatDate(DateTime? date){
+    date ??= DateTime.now();
+    return DateFormat('dd-MMM-yyyy').format(date);
+  }
+}
