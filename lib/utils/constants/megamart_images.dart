389eca2130b1a8e0f72a014ef58fb0dd19247663
skybinary9 <skybinary9@gmail.com>
2025-10-25 09:09:39 +0500

    logo upload

---
+++ lib/utils/constants/megamart_images.dart
@@ -0,0 +1,8 @@
+class MegamartImages {
+  //static const String lightlogo =
+  //static const String lightlogo =
+
+  //---social logo 
+  static const String google = "assets\logos\google.png";
+  static const String facebook = "assets\logos\facebook.png";
+}
