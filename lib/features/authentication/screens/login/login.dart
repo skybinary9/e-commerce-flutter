46ce7d5f206546ec925f81e36b3906c81f81c63a
skybinary9 <skybinary9@gmail.com>
2025-11-10 19:41:05 +0500

    login  screen update

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -10,7 +10,7 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    HelperFunction.isDarkMode(context);
+    final dark = HelperFunction.isDarkMode(context);
 
     return Scaffold(
       body: SingleChildScrollView(
