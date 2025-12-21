8d4ad05d3d29a6c754f53762aa8b973c6f1b7d0b
skybinary9 <skybinary9@gmail.com>
2025-12-21 16:26:03 +0500

    Add custom seachbar

---
+++ lib/common/widgets/custom_shape/container/serachbar.dart
@@ -0,0 +1,40 @@
+import 'package:ecommerce_final_year_project/utils/constants/colors.dart';
+import 'package:ecommerce_final_year_project/utils/constants/size.dart' show MegamartSize;
+import 'package:ecommerce_final_year_project/utils/device/megamart_device_utilty.dart';
+import 'package:ecommerce_final_year_project/utils/helpers/helper_function.dart';
+import 'package:flutter/material.dart';
+
+class AppBarSerachBarContainer extends StatelessWidget {
+  const AppBarSerachBarContainer({
+    super.key, required this.text, this.icon,  this.showBackGround = true,  this.showBorder = true,
+  });
+  final String text;
+  final IconData? icon;
+  final bool showBackGround, showBorder;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = HelperFunction.isDarkMode(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: MegamartSize.defaultSpace),
+      child: Container(
+        width: MegamartDeviceUtility.getScreenwidth(context),
+        padding: const EdgeInsets.all(MegamartSize.md),
+        decoration: BoxDecoration(
+          color: showBackGround ? dark ? MegamartColors.dark : MegamartColors.light : Colors.transparent,
+          borderRadius: BorderRadius.circular(MegamartSize.cardRadiusLg),
+          border: showBorder ? Border.all(color: Colors.grey) : null,
+        ),
+        child: Row(
+          children: [
+            Icon(icon, color: MegamartColors.dGray,),
+            const SizedBox(width: MegamartSize.spaceBetweenItems,),
+            Text(
+              text,
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: dark ? MegamartColors.light : MegamartColors.dGray  ),),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/widget/home.dart
+++ lib/features/shop/screens/widget/home.dart
@@ -1,6 +1,12 @@
 import 'package:ecommerce_final_year_project/common/widgets/custom_shape/container/primary_header_container.dart';
+import 'package:ecommerce_final_year_project/common/widgets/custom_shape/container/serachbar.dart';
 import 'package:ecommerce_final_year_project/features/shop/screens/widget/home_appbar.dart';
+import 'package:ecommerce_final_year_project/utils/constants/colors.dart';
+import 'package:ecommerce_final_year_project/utils/constants/size.dart';
+import 'package:ecommerce_final_year_project/utils/device/megamart_device_utilty.dart';
+import 'package:ecommerce_final_year_project/utils/helpers/helper_function.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -15,8 +21,11 @@ class HomeScreen extends StatelessWidget {
               child: Column(
                children: [
                 //home appbar
-                HomeAppBar()
+                HomeAppBar(),
+                SizedBox(height: MegamartSize.spaceBetweenItems,),
                 //search bar
+                AppBarSerachBarContainer(text: 'Sreach Product', icon: Iconsax.search_normal,),
+                SizedBox(height: MegamartSize.spaceBetweenSections,),
                 //category list
                ],
               
