75eb01123790be40a17589247f1d81c5bb079732
skybinary9 <skybinary9@gmail.com>
2025-12-25 11:17:43 +0500

    user signup firbase create

--- lib/features/authentication/controllers/onboarding/onboarding_controller.dart
+++ lib/features/authentication/controllers/onboarding/onboarding_controller.dart
@@ -1,5 +1,4 @@
 import 'package:ecommerce_final_year_project/features/authentication/screens/login/login.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
